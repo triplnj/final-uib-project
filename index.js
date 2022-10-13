acd913befa2dac3facdef87025f68d4380d91dcb
triplnj <ivkovicd69@gmail.com>
2022-10-13 09:31:51 +0200

    correction

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const path = require('path')
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'docs')));
 app.get('/', function(request,response) {
-response.sendFile(path.join(__dirname, 'public/html/index.html'));
+response.sendFile(path.join(__dirname, 'docs/html/index.html'));
 
 });
 
 app.listen(8000);
+console.log("Listens on localhost:8000");
